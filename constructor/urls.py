a440754c63712d24782d8ab9a32380006df273ed
CrisMadrid23 <cristellemadrid04@gmail.com>
2025-09-29 22:59:00 -0500

    HomePage

--- constructor/urls.py
+++ constructor/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from homepage.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home , name="home"),
     path('questions/', include('questions.urls')),
 ]
 
